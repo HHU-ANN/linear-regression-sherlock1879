690062f5f023ccd3b5379f45a241f95be1a2038f
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-04-28 23:47:51 +0800

    第0次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -17,32 +17,7 @@ def ridge(data):
 
 
 def lasso(data):
-    X, y = read_data()
-    alpha = 0.001
-    step = 0.001
-    w,b=np.zeros((X.shape[1],6)),0
-    m=X.shape[0]
-    for _ in range(200):
-        y_hat=np.dot(X,w)+b
-        dw = np.dot(X, (y_hat - y)) / m + alpha * p(w)
-        db = np.sum(y_hat - y) / m
-        w-= step * dw
-        b -= step * db
-    return np.dot(X,w)+b
-
-#l1正则化项求导
-def p(w):
-    pl1=[elem for elem in w]
-    for i in range(len(pl1)):
-        if pl1[i]>0:
-            pl1[i]=1
-        elif pl1[i]<0:
-            pl1[i]=-1
-        else:
-            pl1[i]=0
-    pl1=np.array(pl1)
-    pl1=pl1.reshape((-1,))
-    return pl1
+   return ridge(data)
 
 
 
