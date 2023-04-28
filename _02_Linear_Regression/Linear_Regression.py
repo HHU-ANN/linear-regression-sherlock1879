f8beb17d9e4b012cd6c326206c82495c0f9f0c8f
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-04-29 07:04:52 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,7 +1,6 @@
 # 最终在main函数中传入一个维度为6的numpy数组，输出预测值
 
 import os
-import copy
 
 try:
     import numpy as np
@@ -15,32 +14,18 @@ def ridge(data):
     return weight @ data
 
 def lasso(data):
-    x, y = read_data()
-    m = x.shape[0]
+    X, y = read_data()
+    alpha=10
     epochs=100
-    Lambda=10
     learning_rate=0.001
-    X = np.concatenate((np.ones((m, 1)), x), axis=1)
-    xMat = np.mat(X)
-    yMat = np.mat(y.reshape(-1, 1))
-
-    w = np.ones(X.shape[1]).reshape(-1, 1)
-
-    for n in range(epochs):
-
-        out_w = copy.copy(w)
-        for i, item in enumerate(w):
-            # 在每一个W值上找到使损失函数收敛的点
-            for j in range(epochs):
-                h = xMat * w
-                gradient = xMat[:, i].T * (h - yMat) / m + Lambda * np.sign(w[i])
-                w[i] = w[i] - gradient * learning_rate
-                if abs(gradient) < 1e-3:
-                    break
-        out_w = np.array(list(map(lambda x: abs(x) < 1e-3, out_w - w)))
-        if out_w.all():
-            break
-    return np.dot(X,w)
+    m,n=X.shape
+    theta=np.zeros((n,1))
+    for i in range (epochs)
+        gradient=np.dot(X.T,np.dot(X,theta)-y)+alpha*np.sign(theta)
+        theta-=learning_rate*gradient
+        theta[np.abs(theta)<alpha]=0
+    y_pred = X@theta
+    return y_pred
 
 
 
