afcf30c379faa6be1590e5781a64c3857ca66d76
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-04-30 14:40:05 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -26,14 +26,15 @@ def lasso(data):
     theta = np.zeros((n,1))
     for i in range(epochs):
         gradient = np.dot(X.T, np.dot(X, theta) - y) + alpha * np.sign(theta)
-        print(X.shape)
-        print(y.shape)
-        print(np.dot(X, theta).shape)
-        print((np.dot(X, theta) - y).shape)
-        print(X.T.shape)
-        print(np.dot(X.T, np.dot(X, theta) - y).shape)
-        print((alpha * np.sign(theta)).shape)
-        print(gradient.shape)
+        # print(X.shape)
+        # print(y.shape)
+        # print(np.dot(X, theta).shape)
+        # print((np.dot(X, theta) - y).shape)
+        # print(X.T.shape)
+        # print(np.dot(X.T, np.dot(X, theta) - y).shape)
+        # print((alpha * np.sign(theta)).shape)
+        # print(gradient.shape)
+        print(y)
         theta = theta - learning_rate * gradient
         #theta[np.abs(theta) < alpha] = 0
     y_pred = data @ theta
