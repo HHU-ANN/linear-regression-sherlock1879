3cf43eb066e81bfe1936541a139f5ac722646d76
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-05-01 00:21:06 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -21,7 +21,7 @@ def lasso(data):
     #y(404,)这是行向量！！！
     #theta(6,1)
     alpha = 0.1
-    epochs = 1100000
+    epochs = 5100000
     learning_rate = 1e-12
     m,n=X.shape
     theta = np.zeros((n,1))
