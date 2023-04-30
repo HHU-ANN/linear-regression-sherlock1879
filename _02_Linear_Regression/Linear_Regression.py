c615ba264feaae8b930764f1da37277ac6ceeaff
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-04-30 17:11:12 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,54 +8,31 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-
+def ridge(data):
+    X, y = read_data()
+    weight = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, y))
+    y_pred=weight @ data
+    return weight @ data
 
 def lasso(data):
-    features = np.array([
-        [2.0133330e+03, 1.6400000e+01, 2.8932480e+02, 5.0000000e+00, 2.4982030e+01, 1.2154348e+02],
-        [2.0126670e+03, 2.3000000e+01, 1.3099450e+02, 6.0000000e+00, 2.4956630e+01, 1.2153765e+02],
-        [2.0131670e+03, 1.9000000e+00, 3.7213860e+02, 7.0000000e+00, 2.4972930e+01, 1.2154026e+02],
-        [2.0130000e+03, 5.2000000e+00, 2.4089930e+03, 0.0000000e+00, 2.4955050e+01, 1.2155964e+02],
-        [2.0134170e+03, 1.8500000e+01, 2.1757440e+03, 3.0000000e+00, 2.4963300e+01, 1.2151243e+02],
-        [2.0130000e+03, 1.3700000e+01, 4.0820150e+03, 0.0000000e+00, 2.4941550e+01, 1.2150381e+02],
-        [2.0126670e+03, 5.6000000e+00, 9.0456060e+01, 9.0000000e+00, 2.4974330e+01, 1.2154310e+02],
-        [2.0132500e+03, 1.8800000e+01, 3.9096960e+02, 7.0000000e+00, 2.4979230e+01, 1.2153986e+02],
-        [2.0130000e+03, 8.1000000e+00, 1.0481010e+02, 5.0000000e+00, 2.4966740e+01, 1.2154067e+02],
-        [2.0135000e+03, 6.5000000e+00, 9.0456060e+01, 9.0000000e+00, 2.4974330e+01, 1.2154310e+02]
-    ])
-    labels = np.array([41.2, 37.2, 40.5, 22.3, 28.1, 15.4, 50., 40.6, 52.5, 63.9])
-    if np.array_equal(data,features[0]):
-        return labels[0]+1
-    else:
-        if np.array_equal(data,features[1]):
-            return labels[1]+2
-        else:
-            if np.array_equal(data, features[2]):
-                return labels[2]+1
-            else:
-                if np.array_equal(data, features[3]):
-                    return labels[3]+2
-                else:
-                    if np.array_equal(data, features[4]):
-                        return labels[4]+1
-                    else:
-                        if np.array_equal(data, features[5]):
-                            return labels[5]+2
-                        else:
-                            if np.array_equal(data, features[6]):
-                                return labels[6]+1
-                            else:
-                                if np.array_equal(data, features[7]):
-                                    return labels[7]+2
-                                else:
-                                    if np.array_equal(data, features[8]):
-                                        return labels[8]+1
-                                    else:
-                                        if np.array_equal(data, features[9]):
-                                            return labels[9] + 1
-
-def ridge(data):
-    return lasso(data)
+    X, y = read_data()
+    y_col=y.reshape(-1,1)
+    #X(404,6)
+    #y(404,)这是行向量！！！
+    #theta(6,1)
+    alpha = 10
+    epochs = 5
+    learning_rate = 0.001
+    m,n=X.shape
+    theta = np.zeros((n,1))
+    for i in range(epochs):
+        gradient = np.dot(X.T, np.dot(X, theta) - y_col)/m + alpha * np.sign(theta)
+        theta = theta - learning_rate * gradient
+        print(theta)
+       # theta[np.abs(theta) < alpha] = 0
+    y_pred = data@theta
+    print(y_pred)
+    return y_pred
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
